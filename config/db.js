a26d5695f52552016a1e3ea586a3e1f10250009a
manojmksmanu <manoj2022019@gmail.com>
2024-08-19 23:23:43 +0530

    db url

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/?retryWrites=true&w=majority&appName=nativechat"
-      "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/chatDB?retryWrites=true&w=majority&appName=nativechat"
+      "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/?retryWrites=true&w=majority&appName=nativechat"
+      // "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/chatDB?retryWrites=true&w=majority&appName=nativechat"
     );
     console.log("MongoDB connected");
   } catch (error) {
