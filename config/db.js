4eeafb0739b380205d1dcc2ad65b1b46e9c1be7b
manojmksmanu <manoj2022019@gmail.com>
2024-08-19 21:23:55 +0530

    database url changes

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/?retryWrites=true&w=majority&appName=nativechat"
+      // "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/?retryWrites=true&w=majority&appName=nativechat"
+       "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/?retryWrites=true&w=majority&appName=nativechat/chatnative"
     );
     console.log("MongoDB connected");
   } catch (error) {
