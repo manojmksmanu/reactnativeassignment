c6c0056e038b5177d30720fc185c5fe2ebed9bdb
manojmksmanu <manoj2022019@gmail.com>
2024-08-20 11:00:57 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/?retryWrites=true&w=majority&appName=nativechat"
+      "mongodb+srv://Manoj4753:Manoj4753@mymegaminds.76midbs.mongodb.net/test?retryWrites=true&w=majority&appName=Mymegaminds"
       // "mongodb+srv://manoj2022019:7gxePcOGu7SnvvVL@nativechat.unnjx.mongodb.net/chatDB?retryWrites=true&w=majority&appName=nativechat"
     );
     console.log("MongoDB connected");
