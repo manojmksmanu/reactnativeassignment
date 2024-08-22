ec396399c203da6feb8592efe62ed97b14ef223f
manojmksmanu <manoj2022019@gmail.com>
2024-08-22 15:22:18 +0530

    dsf

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  sender: { type: String, required: true },
   receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    type:String,
     required: true,
   },
   message: { type: String, required: true },
